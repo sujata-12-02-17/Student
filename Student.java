ba72b2fe5cd9e1b9f7e6b4470b6caf81bfae28e1
sujata swami <sujataswami12@gmail.com>
2020-10-02 11:45:38 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student{
+
+    int Roll_no;
+    String name;
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.Roll_no=1;
+        s.name="sujata";
+
+    }
+
+
+
+}
